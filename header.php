91eb43a023fa53e116e804dee5c7550e3a394661
Jag <jag.jagannath@gmail.com>
2023-07-18 12:33:29 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class( 'antialiased flex flex-col min-h-screen' ); ?>>
+<?php wp_body_open(); ?>
+
+<header class="header">
+	<h1>
+		<a class="font-bold text-blue-500 hover:underline" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<?php bloginfo( 'name' ); ?>
+		</a>
+	</h1>
+</header>
+
+<div class="lg:flex grow">
+	<main id="primary" class="grow p-8" role="main">
